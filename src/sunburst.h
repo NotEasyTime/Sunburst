b73220351ae9cf8caff06a42d32fd5a04c5b68ef
Gabe Parry <91580593+NotEasyTime@users.noreply.github.com>
2025-10-25 10:38:43 -0400

    clean api

--- src/sunburst.h
+++ src/sunburst.h
@@ -14,19 +14,6 @@
 
 typedef struct Color { float r, g, b, a; } Color;
 
-typedef struct {
-    GLuint id;
-    int width;
-    int height;
-} Texture;
-
-typedef struct {
-    // TODO: add clay id
-    bool *value;
-    float size_px;
-    float gap_px;
-} Checkbox;
-
 // Diagnostics
 void PrintFrameRate(void);
 
@@ -38,12 +25,8 @@ void Begin2D(int ,int);
 void End2D(void);
 void RendererInit(void);
 void RendererShutdown(void);
-Texture LoadTexture(const char*);
-void DestroyTexture(Texture*);
-void DrawTexture(Texture*, int, int, int, int, bool);
 
 // Clay 
-//void UI_Checkbox(RGFW_window*, Checkbox, bool);
 void HandleClayErrors(Clay_ErrorData);
 
 // GLFW
